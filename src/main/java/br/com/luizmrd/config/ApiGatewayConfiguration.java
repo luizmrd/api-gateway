8af57d170147d533fea112c68b5b8268ed4f1de0
luizmrd <lipesilvamiranda@hotmail.com>
2025-09-13 11:27:28 -0300

    new logging filter

---
+++ src/main/java/br/com/luizmrd/config/ApiGatewayConfiguration.java
@@ -0,0 +1,24 @@
+package br.com.luizmrd.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+    @Bean
+    public RouteLocator getewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+//                .route(p -> p.path("/get")
+//                        .filters(f -> f
+//                                .addRequestHeader("Hello", "World")
+//                                .addRequestParameter("Hello", "World")
+//                        )
+//                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/book-service/**").uri("lb://book-service"))
+                .route(p -> p.path("/exchange-service/**").uri("lb://exchange-service"))
+                .build();
+
+    }
+}
